43def535d337d4c8432f16df3ebdc21adec599f5
myckgoncalves <myck.pessoal@gmail.com>
2019-10-01 05:50:46 -0300

    Product list by category and table update

--- admin_area/view_cats.php
+++ admin_area/view_cats.php
@@ -1,14 +1,19 @@
 
-<table width="795" align="center" bgcolor="pink"> 
+<table class="view_pro_table">
 
-	
 	<tr align="center">
 		<td colspan="6"><h2>View All Categories Here</h2></td>
 	</tr>
+
+
+<style>
+.view_pro_table td a { text-decoration:none}
+</style>
 	
 	<tr align="center" bgcolor="skyblue">
 		<th>Category ID</th>
 		<th>Category Title</th>
+		<th>Products in category</th>
 		<th>Edit</th>
 		<th>Delete</th>
 	</tr>
@@ -26,17 +31,32 @@
 		$cat_id = $row_cat['cat_id'];
 		$cat_title = $row_cat['cat_title'];
 		$i++;
+
+
+
+if ($result = mysqli_query($con, "SELECT product_id FROM products WHERE product_cat = '$cat_id' ")) {
+
+$row_cnt = mysqli_num_rows($result);
+
+
+}
+
 	
 	?>
+
+
 	<tr align="center">
 		<td><?php echo $i;?></td>
 		<td><?php echo $cat_title;?></td>
-		<td><a href="index.php?edit_cat=<?php echo $cat_id; ?>">Edit</a></td>
-		<td><a href="delete_category.php?delete_cat=<?php echo $cat_id;?>">Delete</a></td>
-	
+		<td><?php echo $row_cnt ?></td>
+		<td>
+		<a href="index.php?edit_cat=<?php echo $cat_id; ?>" class="btn btn-info" role="button">Edit</a>
+		<td>
+		<a href="delete_category.php?delete_cat=<?php echo $cat_id;?>" class="btn btn-danger" role="button">Delete</a>
 	</tr>
-	<?php } ?>
 
+
+<?php } ?>
 
 
 
